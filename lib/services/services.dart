ed436bf2c32b95643f47a061b8ad3c17173fcd67
YogeshKumarYadav <001yadavrahul100@gmail.com>
2023-05-24 01:13:05 +0530

    fixes

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -8,46 +8,47 @@ import '../widgets/text_widget.dart';
 class Services {
   static Future<void> showModeSheet({required BuildContext context}) async {
     await showModalBottomSheet(
-        backgroundColor: scaffoldBackgroundColor,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
-        context: context,
-        builder: (context) {
-          return Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Column(mainAxisSize: MainAxisSize.min, children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Flexible(
-                        child: Text("Choose Mode",
-                            style:
-                                TextStyle(fontSize: 16, color: Colors.white))),
-                    Flexible(flex: 2, child: ModeDropDown()),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.black,
-                      elevation: 2,
-                      shadowColor: Colors.grey,
-                    ),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return UpdateAPIWidget();
-                        },
-                      );
-                    },
-                    child: const Text("Change API",
-                        style: TextStyle(fontSize: 15, color: Colors.white))
-                    )
-              ])
-            );
-        });
+      backgroundColor: scaffoldBackgroundColor,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+      context: context,
+      builder: (context) {
+        return Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Flexible(
+                      child: Text("Choose Mode",
+                          style:
+                              TextStyle(fontSize: 16, color: Colors.white))),
+                  Flexible(flex: 2, child: ModeDropDown()),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.black,
+                    elevation: 2,
+                    shadowColor: Colors.grey,
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return UpdateAPIWidget();
+                      },
+                    );
+                  },
+                  child: const Text("Change API",
+                      style: TextStyle(fontSize: 15, color: Colors.white))
+                  )
+          ])
+        );
+      }
+    );
   }
 }
 
